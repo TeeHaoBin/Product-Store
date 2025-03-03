5b8e7dc34427eadd91a6b9a1f86dd7276ab655ca
TeeHaoBin <haobintee@gmail.com>
2025-03-03 16:24:32 +0800

    Add BrowserRouter to main.jsx and update dependencies

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,5 @@
 import React, { StrictMode } from 'react'
+import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { ChakraProvider } from "@chakra-ui/react"
