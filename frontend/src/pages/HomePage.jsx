cdc17106d14a7de6d23e4e43aae8df7e7497a886
TeeHaoBin <125014451+TeeHaoBin@users.noreply.github.com>
2025-03-09 17:51:32 +0800

    Created & Tested fetchProducts for HomePage
    When HomePage.jsx is opened, fetchProducts will fetch all the products from DB.

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,16 @@
-import { Container, VStack, Text } from '@chakra-ui/react'
+import { Container, VStack, Text, SimpleGrid } from '@chakra-ui/react'
 import { Link } from 'react-router-dom'
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useProductStore } from '../store/product'
 
 const HomePage = () => {
+  const { fetchProducts, products } = useProductStore();
+
+  useEffect(() => {
+    fetchProducts();
+  }, [fetchProducts]);
+  console.log("Products: ", products);
+
   return (
     <Container maxW='container.x1' py={12}>
       <VStack spacing={8}>
@@ -15,6 +23,18 @@ const HomePage = () => {
 				>
 					Current Products 🚀
 				</Text>
+
+        <SimpleGrid
+          columns={{ 
+            base: 1,
+            md: 2,
+            lg: 3,
+          }}
+          spacing={10}
+          w={"full"}
+        >
+
+        </SimpleGrid>
 
         <Text fontSize='xl' textAlign={"center"} fontWeight='bold' color='gray.500'>
 						No products found 😢{" "}
